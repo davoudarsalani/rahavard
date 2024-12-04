16612219dea989b59c976f8a2d15ac9022dfe762
Davoud Arsalani <d_arsalani@yahoo.com>
2024-12-04 21:53:46 +0330

    files: added

---
+++ files/del-reg-key.bat
@@ -0,0 +1,4 @@
+:: >>> NOTE: Use Double Quotes ONLY
+C:\del-reg-key\venv\Scripts\python.exe C:\del-reg-key\del-reg-key.py ^
+--path="SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" ^
+--key="L$RTMTIMEBOMB_1320153D-8DA3-4e8e-B27B-0D888223A588"
